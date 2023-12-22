958f115e6facfc028331ec03ea0f6e586bccfc62
sivakg2000 <sivakg2000@gmail.com>
2023-12-23 05:01:25 +0530

    Create SecondSmallestNumber.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/SecondSmallestNumber.java
@@ -0,0 +1,21 @@
+package com.siva.apps.practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SecondSmallestNumber {
+
+    public static void  main(String[] args){
+        List<Integer> numbers = Arrays.asList(10, 5, 8, 2,3, 12, 7);
+
+       int result=new SecondSmallestNumber().find(numbers);
+       System.out.println(result);
+
+
+    }
+
+    public int find(List<Integer> list){
+        return list.stream().distinct().sorted().skip(1).findFirst().orElseThrow(()-> new IllegalArgumentException("List is too short"));
+    }
+}
