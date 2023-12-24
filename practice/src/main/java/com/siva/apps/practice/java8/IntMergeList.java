37683dbf8efa632a218e691c39ef874452bb053e
sivakg2000 <sivakg2000@gmail.com>
2023-12-25 05:29:21 +0530

    added test for int merge

--- practice/src/main/java/com/siva/apps/practice/java8/IntMergeList.java
+++ practice/src/main/java/com/siva/apps/practice/java8/IntMergeList.java
@@ -2,6 +2,8 @@ package com.siva.apps.practice.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class IntMergeList {
 
@@ -12,10 +14,14 @@ public class IntMergeList {
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
         List<Integer> list2 = Arrays.asList(5, 6, 7, 8);
 
+        List<Integer> mList=new IntMergeList().merge(list1,list2);
+
+        System.out.println((mList));
+
 
     }
 
     public List<Integer>  merge(List<Integer> list1,List<Integer> list2){
-        return null;
+        return IntStream.range(0,Math.min(list1.size(),list1.size())).mapToObj(i->Arrays.asList(list1.get(i),list2.get(i))).flatMap(List::stream).collect(Collectors.toList());
     }
 }
--- practice/src/test/java/IntMergeListTest.java
+++ practice/src/test/java/IntMergeListTest.java
@@ -1,3 +1,64 @@
+import com.siva.apps.practice.java8.IntMergeList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IntMergeListTest {
+
+    @Test
+    public  void  merge1(){
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
+        List<Integer> list2 = Arrays.asList(5, 6, 7, 8);
+
+        List<Integer> rList = Arrays.asList(1,5, 2,6, 3,7, 4,8);
+
+        List<Integer> mList=new IntMergeList().merge(list1,list2);
+
+        assertEquals(mList,rList);
+
+
+    }
+
+
+    @Test
+    public  void  merge2(){
+
+        List<Integer> list1 = Arrays.asList(10, 20, 30, 40, 50);
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+
+        List<Integer> rList = Arrays.asList(10,1, 20,2, 30,3, 40,4, 50,5);
+
+        List<Integer> mList=new IntMergeList().merge(list1,list2);
+
+        assertEquals(mList,rList);
+
+
+    }
+
+
+    @Test
+    public  void  merge3(){
+
+
+        List<Integer> list1 = Arrays.asList(100, 200, 300);
+        List<Integer> list2 = Arrays.asList(5, 10, 15, 20, 25);
+
+
+        List<Integer> rList = Arrays.asList(100,5, 200,10, 300,15);
+
+        List<Integer> mList=new IntMergeList().merge(list1,list2);
+
+        assertEquals(rList,mList);
+
+
+    }
+
+
+
 
 }
