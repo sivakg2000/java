190bf808ba6fb133614a03449047d3a2c6c34159
sivakg2000 <sivakg2000@gmail.com>
2023-12-25 05:13:53 +0530

    sample

--- practice/src/main/java/com/siva/apps/practice/java8/IntMergeList.java
+++ practice/src/main/java/com/siva/apps/practice/java8/IntMergeList.java
@@ -9,18 +9,13 @@ public class IntMergeList {
 
     public static void  main(String[] args){
 
-
-
         List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
         List<Integer> list2 = Arrays.asList(5, 6, 7, 8);
-
-
-
 
 
     }
 
     public List<Integer>  merge(List<Integer> list1,List<Integer> list2){
-        return null
+        return null;
     }
 }
--- practice/src/main/java/com/siva/apps/practice/java8/NumCharMap.java
+++ practice/src/main/java/com/siva/apps/practice/java8/NumCharMap.java
@@ -1,4 +1,24 @@
 package com.siva.apps.practice.java8;
 
+import java.util.*;
+
 public class NumCharMap {
+
+
+    public static void  main(String[] args){
+
+        String text = "hellojava";
+        System.out.println(new NumCharMap().find(text));
+
+
+    }
+
+    public Map find(String str){
+
+        Map<Character,Integer> rVal=new HashMap<>();
+        str.chars().mapToObj(c->(char)c).forEach(c->rVal.put(c,rVal.getOrDefault(c,0)+1));
+        return rVal;
+    }
+
+
 }
