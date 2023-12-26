007de6fc2c449b49d7bde04c495d9638c033de19
sivakg2000 <sivakg2000@gmail.com>
2023-12-27 04:54:11 +0530

    Update Len5UpperCaseTest.java

--- practice/src/test/java/Len5UpperCaseTest.java
+++ practice/src/test/java/Len5UpperCaseTest.java
@@ -1,2 +1,42 @@
+import com.siva.apps.practice.java8.Len5UpperCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Len5UpperCaseTest {
+
+    @Test
+    public void find1(){
+        List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "strawberry", "grapes");
+        String expResult="BANANA";
+        List<String> fList=new Len5UpperCase().find(words);
+        assertTrue(fList.contains(expResult));
+
+
+    }
+
+
+
+    @Test
+    public void find2(){
+        List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "strawberry", "grapes");
+        String expResult="STRAWBERRY";
+        List<String> fList=new Len5UpperCase().find(words);
+        assertTrue(fList.contains(expResult));
+
+
+    }
+
+
+    @Test
+    public void findEmpty(){
+        List<String> words = Collections.emptyList();
+        List<String> fList=new Len5UpperCase().find(words);
+        assertTrue(fList.isEmpty());
+
+
+    }
 }
