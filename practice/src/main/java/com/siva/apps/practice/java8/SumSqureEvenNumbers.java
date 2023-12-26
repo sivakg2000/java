53d2d96f45565aafbec0554b15c0f18918c1f4bd
sivakg2000 <sivakg2000@gmail.com>
2023-12-27 04:41:56 +0530

    Problem 1

--- practice/src/main/java/com/siva/apps/practice/java8/SumSqureEvenNumbers.java
+++ practice/src/main/java/com/siva/apps/practice/java8/SumSqureEvenNumbers.java
@@ -1,4 +1,20 @@
 package com.siva.apps.practice.java8;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class SumSqureEvenNumbers {
+
+    public static void  main(String[] args){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        new SumSqureEvenNumbers().find(numbers);
+
+    }
+
+    public int find(List<Integer> list){
+        return list.stream().filter(num->num%2==0).map(n->n*n).mapToInt(v->v).sum();
+         
+    }
+
 }
--- practice/src/test/java/SumSqureEvenNumbersTest.java
+++ practice/src/test/java/SumSqureEvenNumbersTest.java
@@ -1,2 +1,53 @@
+import com.siva.apps.practice.java8.SumSqureEvenNumbers;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SumSqureEvenNumbersTest {
+    @Test
+    public  void  find1(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        int expVal=220;
+        int rVal=new SumSqureEvenNumbers().find(numbers);
+        assertEquals(expVal,rVal);
+
+
+    }
+
+
+    @Test
+    public  void  find2(){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int expVal=120;
+        int rVal=new SumSqureEvenNumbers().find(numbers);
+        assertEquals(expVal,rVal);
+
+
+    }
+
+
+    @Test
+    public  void  find3(){
+        List<Integer> numbers = Arrays.asList(1, 2, 5, 6, 7);
+        int expVal=40;
+        int rVal=new SumSqureEvenNumbers().find(numbers);
+        assertEquals(expVal,rVal);
+
+
+    }
+
+
+
+    @Test
+    public  void  findEmpty(){
+        List<Integer> numbers = Collections.emptyList();
+        int expVal=0;
+        int rVal=new SumSqureEvenNumbers().find(numbers);
+        assertEquals(expVal,rVal);
+
+
+    }
 }
