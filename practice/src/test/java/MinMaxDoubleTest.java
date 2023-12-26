efcccf876d24fb70bc26af0d1c82b80608ec08ed
sivakg2000 <sivakg2000@gmail.com>
2023-12-26 10:25:07 +0530

    Update MinMaxDoubleTest.java

--- practice/src/test/java/MinMaxDoubleTest.java
+++ practice/src/test/java/MinMaxDoubleTest.java
@@ -2,6 +2,7 @@ import com.siva.apps.practice.java8.MinMaxDouble;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,8 +27,36 @@ public class MinMaxDoubleTest {
 
         MinMaxDouble.MinMaxResult expResult=new MinMaxDouble.MinMaxResult(3.9,12.1);
         MinMaxDouble.MinMaxResult rVal=new MinMaxDouble().find(doubles);
- 
+
         assertEquals(expResult.getMaxVal(),rVal.getMaxVal());
 
     }
+
+
+
+    @Test
+    public  void findEmptyMin(){
+        List<Double> doubles = Collections.emptyList();
+
+        MinMaxDouble.MinMaxResult expResult=new MinMaxDouble.MinMaxResult(Double.NaN,Double.NaN);
+        MinMaxDouble.MinMaxResult rVal=new MinMaxDouble().find(doubles);
+
+        assertEquals(expResult.getMinVal(),rVal.getMinVal());
+
+    }
+
+
+    @Test
+    public  void findEmptyMax(){
+        List<Double> doubles = Collections.emptyList();
+
+        MinMaxDouble.MinMaxResult expResult=new MinMaxDouble.MinMaxResult(Double.NaN,Double.NaN);
+        MinMaxDouble.MinMaxResult rVal=new MinMaxDouble().find(doubles);
+
+        assertEquals(expResult.getMaxVal(),rVal.getMaxVal());
+
+    }
+
+
+
 }
