c6250f4655c280ce41f344e67651173c38076498
sivakg2000 <sivakg2000@gmail.com>
2023-12-27 04:28:08 +0530

    SumSqureEvenNumbers

--- practice/src/test/java/NumWordMapTest.java
+++ practice/src/test/java/NumWordMapTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals; 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumWordMapTest {
     @Test
