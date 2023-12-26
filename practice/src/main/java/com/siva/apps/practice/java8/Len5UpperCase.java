1f580b46c8ba8208a0825211b1440ce7297fc50a
sivakg2000 <sivakg2000@gmail.com>
2023-12-27 04:47:10 +0530

    Update Len5UpperCase.java

--- practice/src/main/java/com/siva/apps/practice/java8/Len5UpperCase.java
+++ practice/src/main/java/com/siva/apps/practice/java8/Len5UpperCase.java
@@ -1,4 +1,19 @@
 package com.siva.apps.practice.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Len5UpperCase {
+
+    public static void main(String[] args){
+        List<String> words = Arrays.asList("apple", "banana", "orange", "kiwi", "strawberry", "grapes");
+        List<String> fList=new Len5UpperCase().find(words);
+        System.out.println(fList);
+
+    }
+
+    public List<String> find(List<String> list){
+        return list.stream().filter(word->word.length()>5).map(String::toUpperCase).collect(Collectors.toList());
+    }
 }
