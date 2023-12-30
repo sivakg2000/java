3ad697fa8e04ca8aa8fca03cfbd013c358933edb
sivakg2000 <sivakg2000@gmail.com>
2023-12-31 05:29:30 +0530

    Update UniqueCharList.java

--- practice/src/main/java/com/siva/apps/practice/java8/UniqueCharList.java
+++ practice/src/main/java/com/siva/apps/practice/java8/UniqueCharList.java
@@ -1,4 +1,18 @@
 package com.siva.apps.practice.java8;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class UniqueCharList {
+    public static void main(String[] args){
+        List<String> words = Arrays.asList("hello", "world", "java", "stream", "example", "unique", "characters");
+        List<String> fList=new UniqueCharList().find(words);
+        System.out.println(fList);
+
+    }
+
+    public List<String> find(List<String> list){
+        return list.stream().filter(word->  word.length()==   word.chars().distinct().count()).collect(Collectors.toList());
+    }
 }
