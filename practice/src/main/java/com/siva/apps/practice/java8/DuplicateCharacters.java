92ee7e6f5615a8527dbc75bb6c452cb0d0b395f8
sivakg2000 <sivakg2000@gmail.com>
2024-01-06 05:10:29 +0530

    Update DuplicateCharacters.java

--- practice/src/main/java/com/siva/apps/practice/java8/DuplicateCharacters.java
+++ practice/src/main/java/com/siva/apps/practice/java8/DuplicateCharacters.java
@@ -1,8 +1,10 @@
 package com.siva.apps.practice.java8;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 
 public class DuplicateCharacters {
     public static void  main(String[] args){
@@ -14,19 +16,34 @@ public class DuplicateCharacters {
         System.out.println(new DuplicateCharacters().find(string2));
         System.out.println(new DuplicateCharacters().find(string3));
 
+        System.out.println(new DuplicateCharacters().findWOStream(string1));
+        System.out.println(new DuplicateCharacters().findWOStream(string2));
+        System.out.println(new DuplicateCharacters().findWOStream(string3));
+
 
 
 
     }
 
     public boolean find(String str){
+        //System.out.println(str);
         System.out.println(str.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().stream().filter(e->e.getValue()>1).collect(Collectors.toList()));
         return false;
     }
 
 
     public boolean findWOStream(String str){
-        System.out.println(str.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())).entrySet().stream().filter(e->e.getValue()>1).collect(Collectors.toList()));
+        //System.out.println(str);
+        Map<Character,Long> characterLongMap=new HashMap<>();
+        for(char c:str.toCharArray()){
+            characterLongMap.put(c,characterLongMap.getOrDefault(c,0L)+1);
+        }
+        //System.out.println(characterLongMap);
+        characterLongMap.forEach((k,v)->{
+            if(v>1)
+            System.out.println(k+":"+v);
+        });
+        System.out.println();
         return false;
     }
 }
