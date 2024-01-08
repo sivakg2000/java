386ba0ef0f594645a0aee1a0fd5d7f435b2091ac
sivakg2000 <sivakg2000@gmail.com>
2024-01-08 08:19:03 +0530

    Create BalancedBrackets.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/BalancedBrackets.java
@@ -0,0 +1,34 @@
+package com.siva.apps.practice.java8;
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+    public static void main(String[] args) {
+        String testString1 = "{[()()]}";
+        String testString2 = "{[()]";
+
+        System.out.println("Is \"" + testString1 + "\" balanced? " + isBalanced(testString1));
+        System.out.println("Is \"" + testString2 + "\" balanced? " + isBalanced(testString2));
+    }
+
+    private static boolean isBalanced(String testString1) {
+
+        Stack<Character> stack= new Stack<>();
+
+        for(char c:testString1.toCharArray()){
+
+            if(c=='(' || c=='[' || c=='{'){
+                stack.push(c);
+            }else if(c==')' && !stack.isEmpty() && stack.peek()=='('){
+                stack.pop();
+            } else if(c==']' && !stack.isEmpty() && stack.peek()=='['){
+                stack.pop();
+            } else if(c=='}' && !stack.isEmpty() && stack.peek()=='{'){
+                stack.pop();
+            }
+
+        }
+        return  stack.isEmpty();
+    }
+
+}
