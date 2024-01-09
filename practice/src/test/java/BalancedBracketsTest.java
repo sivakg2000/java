c3cadc245ed13ea27b6bfddf20c8ff2d5c331b7a
sivakg2000 <sivakg2000@gmail.com>
2024-01-09 22:55:25 +0530

    Update BalancedBracketsTest.java

--- practice/src/test/java/BalancedBracketsTest.java
+++ practice/src/test/java/BalancedBracketsTest.java
@@ -1,2 +1,23 @@
+import com.siva.apps.practice.java8.BalancedBrackets;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class BalancedBracketsTest {
+
+    @Test
+    public  void test1() {
+        String testString1 = "{[()()]}";
+        assertTrue(new BalancedBrackets().isBalanced(testString1));
+
+    }
+
+
+    @Test
+    public  void test1() {
+        String testString1 = "{[()()]}";
+        assertTrue(new BalancedBrackets().isBalanced(testString1));
+
+    }
 }
