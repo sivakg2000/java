dee27dfb1864eec6a588a02c7693a5bc153dbadf
sivakg2000 <sivakg2000@gmail.com>
2024-01-10 10:48:40 +0530

    Create MaximumSubarraySum.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/MaximumSubarraySum.java
@@ -0,0 +1,19 @@
+package com.siva.apps.practice.java8;
+
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSum = maxSubArray(nums);
+        System.out.println("Maximum subarray sum: " + maxSum);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum=nums[0];
+        int currentSum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            currentSum=Math.max(nums[i],currentSum+nums[i]);
+            maxSum=Math.max(maxSum,currentSum);
+        }
+        return  maxSum;
+    }
+}
