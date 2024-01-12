fdac6756699e822c91b5cb7154c66158e2551431
sivakg2000 <sivakg2000@gmail.com>
2024-01-13 03:24:29 +0530

    Create Divisible3A5.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/Divisible3A5.java
@@ -0,0 +1,16 @@
+package com.siva.apps.practice.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Divisible3A5 {
+
+    public static void  main(String[] args){
+ System.out.println(new Divisible3A5().find(100));
+    }
+    public List<Integer> find(int max){
+        return IntStream.range(1,max).filter(n-> n%3==0 && n%5==0).boxed().collect(Collectors.toList()); 
+    }
+}
