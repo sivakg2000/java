36aa392c81148c996f51b36b4709bba3b7a06193
sivakg2000 <sivakg2000@gmail.com>
2024-01-12 06:58:36 +0530

    Update SumPrimeNumbers.java

--- practice/src/main/java/com/siva/apps/practice/java8/SumPrimeNumbers.java
+++ practice/src/main/java/com/siva/apps/practice/java8/SumPrimeNumbers.java
@@ -19,13 +19,15 @@ public class SumPrimeNumbers {
     public int find(List<Integer> list) {
         List<Integer> fList=list.stream().filter(this::isPrime).collect(Collectors.toList());
         System.out.println(fList);
-        return 0;
+        int sum= fList.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+        return sum;
     }
 
     private boolean isPrime(int num){
         if(num<=1)
             return false;
-        for(int i=2;i<Math.sqrt(num);i++){
+        for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i==0)
                 return false;
         }
