2586b65587c3de514bc89dd0295b8537677faba5
sivakg2000 <sivakg2000@gmail.com>
2024-01-14 07:44:39 +0530

    Create IntersectTwoListTest.java

---
+++ practice/src/test/java/IntersectTwoListTest.java
@@ -0,0 +1,17 @@
+import com.siva.apps.practice.java8.J3LString;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntersectTwoListTest {
+
+    @Test
+    public void findJ(){
+
+
+    }
+}
