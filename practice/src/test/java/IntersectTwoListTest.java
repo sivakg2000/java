4dbb1cb907cd7454525d633bb8f2e59340e16c05
sivakg2000 <sivakg2000@gmail.com>
2024-01-14 09:06:39 +0530

    Update IntersectTwoListTest.java

--- practice/src/test/java/IntersectTwoListTest.java
+++ practice/src/test/java/IntersectTwoListTest.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class IntersectTwoListTest {
 
@@ -18,6 +19,17 @@ public class IntersectTwoListTest {
         List<Integer> expAnwserList=Arrays.asList(3, 4, 5);
         List<Integer> anwserList=new IntersectTwoList().find(list1,list2);
         assertEquals(expAnwserList,anwserList);
+    }
+
+
+    @Test
+    public void find2(){
+
+        List<Integer> list1= Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list2= Arrays.asList(3, 4, 5, 6, 7);
+        List<Integer> expAnwserList=Arrays.asList(1, 4, 5);
+        List<Integer> anwserList=new IntersectTwoList().find(list1,list2);
+        assertNotEquals(expAnwserList,anwserList);
 
 
 
