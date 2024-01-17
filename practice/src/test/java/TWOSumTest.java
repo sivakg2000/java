94fde8b90b9390405f6e8101d848ddae14dc049f
sivakg2000 <sivakg2000@gmail.com>
2024-01-17 08:17:17 +0530

    Update TWOSumTest.java

--- practice/src/test/java/TWOSumTest.java
+++ practice/src/test/java/TWOSumTest.java
@@ -1,3 +1,4 @@
+import com.siva.apps.practice.java.TWOSum;
 import com.siva.apps.practice.java8.PrimeNumber;
 import org.junit.jupiter.api.Test;
 
@@ -8,9 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TWOSumTest {
     @Test
-    public  void  find1(){
+    public  void  find(){
 
-
+        int[] nums = {2,7,11,15};
+        int[] resInd=new TWOSum().twoSum(nums,9);
+        int[] expInd={1,0};
+        assertEquals(expInd[0],resInd[0]);
+        assertEquals(expInd[1],resInd[1]);
 
 
     }
