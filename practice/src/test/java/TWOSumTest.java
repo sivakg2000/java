6188fd5f819f22fe41633e1b619ef9fd609a60b7
sivakg2000 <sivakg2000@gmail.com>
2024-01-17 08:14:20 +0530

    Create TWOSumTest.java

---
+++ practice/src/test/java/TWOSumTest.java
@@ -0,0 +1,17 @@
+import com.siva.apps.practice.java8.PrimeNumber;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TWOSumTest {
+    @Test
+    public  void  find1(){
+
+
+
+
+    }
+}
