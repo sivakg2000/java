b9170f282edcb05e82ccba91eebb8b4cc2678f78
sivakg2000 <sivakg2000@gmail.com>
2024-01-19 04:29:37 +0530

    Update BuySellStockTest.java

--- practice/src/test/java/BuySellStockTest.java
+++ practice/src/test/java/BuySellStockTest.java
@@ -1,5 +1,6 @@
 import com.siva.apps.practice.java.BuySellStock;
 import com.siva.apps.practice.java8.AvgLenString;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,10 +10,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BuySellStockTest {
     @Test
-    public void findAvgLen(){
+    public void findProfit1(){
         int[] prices = {7,1,5,3,6,4};
         int expProf=5;
         int pro=new BuySellStock().maxProfit(prices);
+        Assertions.assertEquals(expProf,pro);
     }
+
+
+    @Test
+    public void findProfit2(){
+        int[] prices = {7,2,5,3,6,4};
+        int expProf=4;
+        int pro=new BuySellStock().maxProfit(prices);
+        Assertions.assertEquals(expProf,pro);
+    }
+
 
 }
