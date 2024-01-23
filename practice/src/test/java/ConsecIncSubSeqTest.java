bd3db436991391220f1b3a760f8a7976b71acf93
sivakg2000 <sivakg2000@gmail.com>
2024-01-23 05:36:54 +0530

    Update ConsecIncSubSeqTest.java

--- practice/src/test/java/ConsecIncSubSeqTest.java
+++ practice/src/test/java/ConsecIncSubSeqTest.java
@@ -1,2 +1,13 @@
+import com.siva.apps.practice.java8.SecondSmallestNumber;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ConsecIncSubSeqTest {
+
+    @Test
+    public void find(){ 
+    }
 }
