ed96fc5ab271a0f888c01d87d31bf538ac833bcc
sivakg2000 <sivakg2000@gmail.com>
2024-01-23 05:40:07 +0530

    Update ConsecIncSubSeqTest.java

--- practice/src/test/java/ConsecIncSubSeqTest.java
+++ practice/src/test/java/ConsecIncSubSeqTest.java
@@ -1,3 +1,4 @@
+import com.siva.apps.practice.java8.ConsecIncSubSeq;
 import com.siva.apps.practice.java8.SecondSmallestNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,11 @@ import java.util.List;
 public class ConsecIncSubSeqTest {
 
     @Test
-    public void find(){ 
+    public void find(){
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 5, 6, 7, 8, 10, 11,12);
+        List<List<Integer>> result=new ConsecIncSubSeq().find(list1);
+        List<Integer> expRes= Arrays.asList(1, 2, 3);
+        Assertions.assertEquals(expRes,result.get(0));
     }
 }
