25087cadebba3521d0f3a29e882bbd0944937137
sivakg2000 <sivakg2000@gmail.com>
2024-01-25 14:47:02 +0530

    Update ConsecIncSubSeq.java

--- practice/src/main/java/com/siva/apps/practice/java8/ConsecIncSubSeq.java
+++ practice/src/main/java/com/siva/apps/practice/java8/ConsecIncSubSeq.java
@@ -11,7 +11,7 @@ public class ConsecIncSubSeq {
 
     public static void main (String[] args){
 
-        List<Integer> list1 = Arrays.asList(1, 2, 3, 5, 6, 7, 8, 9,10, 11,12);
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 5, 6, 7, 8, 10, 11,12);
         System.out.println(new ConsecIncSubSeq().find(list1));
 
     }
