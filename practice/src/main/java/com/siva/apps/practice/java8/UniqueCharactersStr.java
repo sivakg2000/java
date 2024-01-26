4c963bd378f3c92f4f039fd448b3a8228004560f
sivakg2000 <sivakg2000@gmail.com>
2024-01-27 05:02:02 +0530

    Update UniqueCharactersStr.java

--- practice/src/main/java/com/siva/apps/practice/java8/UniqueCharactersStr.java
+++ practice/src/main/java/com/siva/apps/practice/java8/UniqueCharactersStr.java
@@ -1,5 +1,9 @@
 package com.siva.apps.practice.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UniqueCharactersStr {
 
 
@@ -10,12 +14,24 @@ public class UniqueCharactersStr {
         boolean result=new UniqueCharactersStr().find(str);
         System.out.println(result);
 
+        List<String> strings = Arrays.asList("hello", "world", "java", "programming", "unique");
+
+        List<String> fList=new UniqueCharactersStr().find(strings);
+
+        System.out.println(fList);
+
 
     }
 
     public boolean find(String str){
 
         return str.length()==str.chars().distinct().count();
+
+    }
+
+    public List<String> find(List<String> list){
+
+       return list.stream().filter(this::find).collect(Collectors.toList());
 
     }
 }
