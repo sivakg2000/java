07fc10629f315c8b143318468749adfe17189558
sivakg2000 <sivakg2000@gmail.com>
2024-01-27 05:08:53 +0530

    Create VowelCount.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/VowelCount.java
@@ -0,0 +1,19 @@
+package com.siva.apps.practice.java8;
+
+public class VowelCount {
+
+    public static void  main(String[] args){
+        String input1 = "programming is fun"; // Vowels: 3, Consonants: 7
+        new VowelCount().find(input1);
+
+
+
+
+    }
+
+    public void find(String str){
+
+        System.out.println(str);
+
+    }
+}
