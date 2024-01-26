f9cea0535c0f3dd8a2af4b92d2fcea39bc441884
sivakg2000 <sivakg2000@gmail.com>
2024-01-27 05:13:04 +0530

    Update VowelCount.java

--- practice/src/main/java/com/siva/apps/practice/java8/VowelCount.java
+++ practice/src/main/java/com/siva/apps/practice/java8/VowelCount.java
@@ -1,9 +1,12 @@
 package com.siva.apps.practice.java8;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class VowelCount {
 
     public static void  main(String[] args){
-        String input1 = "programming is fun"; // Vowels: 3, Consonants: 7
+        String input1 = "programming is fun"; 
         new VowelCount().find(input1);
 
 
@@ -15,5 +18,12 @@ public class VowelCount {
 
         System.out.println(str);
 
+        System.out.println(str.chars().filter(this::isVowel).mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(),Collectors.counting())));
+
+    }
+
+
+    private boolean isVowel(int c){
+        return "aeiou".indexOf(c)!=-1;
     }
 }
