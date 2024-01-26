c80dccede75fe7168211edd853f9102b3283ea6d
sivakg2000 <sivakg2000@gmail.com>
2024-01-26 11:03:59 +0530

    Update ConsecIncSubSeqTest.java

--- practice/src/test/java/ConsecIncSubSeqTest.java
+++ practice/src/test/java/ConsecIncSubSeqTest.java
@@ -42,7 +42,7 @@ public class ConsecIncSubSeqTest {
 
         List<Integer> list1 = Arrays.asList(1, 2, 3, 5, 6, 7, 8,9, 10, 11,12);
         List<List<Integer>> result=new ConsecIncSubSeq().find(list1);
-        List<Integer> expRes= Arrays.asList(5,6,7,8);
+        List<Integer> expRes= Arrays.asList(5,6,7,8,9,10,11,12);
         Assertions.assertEquals(expRes,result.get(1));
     }
 
