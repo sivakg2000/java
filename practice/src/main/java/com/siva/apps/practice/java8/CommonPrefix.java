7d364071f30ee1dc00d023d1bfdcd67543985820
sivakg2000 <sivakg2000@gmail.com>
2024-01-29 13:19:29 +0530

    Update CommonPrefix.java

--- practice/src/main/java/com/siva/apps/practice/java8/CommonPrefix.java
+++ practice/src/main/java/com/siva/apps/practice/java8/CommonPrefix.java
@@ -8,14 +8,27 @@ public class CommonPrefix {
     public static void  main(String[] args){
 
         List<String> words = Arrays.asList("pre", "prefix", "preposition", "prediction");
-        double result=new AvgLenString().find(words);
+        String result=new CommonPrefix().find(words);
         System.out.println(result);
 
 
     }
 
-    public double find(List<String> list){
-        return list.stream().mapToDouble(String::length).average().orElse(0.0);
+    public String find(List<String> list){
+        //return list.stream().mapToDouble(String::length).average().orElse(0.0);
+
+        String s=list.stream().reduce((prefix,word)->{
+
+            int minL=Math.min(prefix.length(),word.length());
+            int comL=0;
+            for(int i=0;i<minL;i++){
+
+            }
+
+            System.out.println(prefix +">>"+word+">>"+minL+">>"+comL);
+            return word;
+        }).orElse("");
+        return s;
     }
-}
+
 }
