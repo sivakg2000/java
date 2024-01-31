34c1aaf39e92f082f32884da55991e124664d7bd
sivakg2000 <sivakg2000@gmail.com>
2024-02-01 04:36:09 +0530

    Update CommonPrefixTest.java

--- practice/src/test/java/CommonPrefixTest.java
+++ practice/src/test/java/CommonPrefixTest.java
@@ -1,2 +1,25 @@
+import com.siva.apps.practice.java8.AvgLenString;
+import com.siva.apps.practice.java8.CommonPrefix;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CommonPrefixTest {
+
+    @Test
+    public void findA(){
+
+
+
+        List<String> words = Arrays.asList("pref", "prefix", "preposition", "prediction");
+        String result=new CommonPrefix().find(words);
+        assertEquals("pre",result);
+
+
+    }
+
+
 }
