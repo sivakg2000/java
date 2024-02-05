b954a93a37dca9097d42272d3fa3a6461cef1080
sivakg2000 <sivakg2000@gmail.com>
2024-02-05 10:21:59 +0530

    Create CollectorsOperations.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/CollectorsOperations.java
@@ -0,0 +1,46 @@
+package com.siva.apps.practice.java8;
+
+import com.siva.apps.practice.java8.bo.Person;
+import org.jeasy.random.EasyRandom;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CollectorsOperations {
+
+    public static void main(String[] args){
+
+
+        Collection<Person> mainList= new ArrayList<>();
+
+        mainList.add(new Person("Ram","G",Person.Gender.MALE,40, "Chennai" ));
+        mainList.add(new Person("Rani","S",Person.Gender.FEMALE,30, "Trichy" ));
+        mainList.add(new Person("Selvam","G",Person.Gender.MALE,39, "Delhi" ));
+        mainList.add(new Person("Saraswathi","L",Person.Gender.FEMALE,29, "Coimbatore" ));
+        mainList.add(new Person("Saraswathi","L",Person.Gender.FEMALE,29, "Coimbatore" ));
+
+        //toList
+
+        List<Person> maleList=mainList.stream().filter(p->p.getGender()==Person.Gender.MALE).collect(Collectors.toList());
+
+        System.out.println(maleList);
+
+        //toSet
+
+        
+        //summarizingInt
+
+        //3 method int,accumulate,combine exo ArrayList new,add,addAll
+
+        //Customer Collectors int,accumulate,combine
+
+        //groupingBy 1 Arg
+        //groupingBy 2 Arg
+        //
+
+
+
+    }
+
+
+}
