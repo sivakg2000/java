6ae5732817fbc2d722a9a5b6ac72d0bfa6cbded3
sivakg2000 <sivakg2000@gmail.com>
2024-02-07 05:13:28 +0530

    Create AggregateOperations.java

---
+++ practice/src/main/java/com/siva/apps/practice/java8/AggregateOperations.java
@@ -0,0 +1,38 @@
+package com.siva.apps.practice.java8;
+
+import com.siva.apps.practice.java8.bo.Person;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class AggregateOperations {
+
+    public static void main(String[] args){
+        Collection<Person> mainList= new ArrayList<>();
+
+        //count
+
+        //average
+
+        //max
+
+        //min
+
+        //sum
+
+
+        //reduce
+        //IntBinaryOperator //maxFemaleAge //lambdaExp minMaleAge //sumFemaleAge
+
+
+
+
+
+
+
+
+    }
+
+
+}
