9d8c1de411849a0e7d9290ef358643bf3bf2546a
sivakg2000 <sivakg2000@gmail.com>
2024-02-07 05:15:11 +0530

    Update AggregateOperations.java

--- practice/src/main/java/com/siva/apps/practice/java8/AggregateOperations.java
+++ practice/src/main/java/com/siva/apps/practice/java8/AggregateOperations.java
@@ -11,7 +11,18 @@ public class AggregateOperations {
     public static void main(String[] args){
         Collection<Person> mainList= new ArrayList<>();
 
+        mainList.add(new Person("Ram","G",Person.Gender.MALE,40, "Chennai" ));
+        mainList.add(new Person("Rani","S",Person.Gender.FEMALE,30, "Trichy" ));
+        mainList.add(new Person("Selvam","G",Person.Gender.MALE,39, "Delhi" ));
+        mainList.add(new Person("Saraswathi","L",Person.Gender.FEMALE,29, "Coimbatore" ));
+        mainList.add(new Person("Saraswathi","L",Person.Gender.FEMALE,29, "Coimbatore" ));
+
+
+
         //count
+
+        long maleCount=mainList.stream().filter(p->p.getGender()== Person.Gender.MALE).count();
+        System.out.println("maleCount :"+maleCount);
 
         //average
 
