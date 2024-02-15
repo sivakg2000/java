c1f3fd90557a3b35c79d7cfa31751a789c03cc90
sivakg2000 <sivakg2000@gmail.com>
2024-02-16 05:16:31 +0530

    Create BlogRepo.java

---
+++ SimpleRestAPI/src/main/java/com/siva/apps/SimpleRestAPI/repository/BlogRepo.java
@@ -0,0 +1,7 @@
+package com.siva.apps.SimpleRestAPI.repository;
+
+import com.siva.apps.SimpleRestAPI.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepo  extends JpaRepository<Blog,Integer> {
+}
