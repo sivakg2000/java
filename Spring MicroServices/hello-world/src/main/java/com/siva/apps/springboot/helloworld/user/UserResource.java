40b43f5462f9ad9c132f08cd2992d515614c8801
sivakg2000 <sivakg2000@gmail.com>
2024-03-03 05:29:20 +0530

    Update UserResource.java

--- Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/user/UserResource.java
+++ Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/user/UserResource.java
@@ -1,8 +1,6 @@
 package com.siva.apps.springboot.helloworld.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,22 @@ public class UserResource {
     @GetMapping(path = "/users/{id}")
     public User getUser(@PathVariable int id){
         return this.service.findById(id);
+    }
+
+
+    @PostMapping(path = "/users")
+    public User addUser(@RequestBody User newUser){
+        return this.service.saveUser(newUser);
+    }
+
+    @PutMapping(path = "/users/{id}")
+    public User updateUser(@PathVariable int id,@RequestBody User updateUser){
+        return this.service.updateUser(updateUser,id);
+    }
+
+
+    @DeleteMapping(path = "/users/{id}")
+    public List<User> deleteUser(@PathVariable int id){
+        return this.service.deleteUser(id);
     }
 }
