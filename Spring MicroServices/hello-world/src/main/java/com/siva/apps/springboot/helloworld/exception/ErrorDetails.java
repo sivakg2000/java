21d73b7fbbd0a90703125b547f45adb1293c9c1c
sivakg2000 <sivakg2000@gmail.com>
2024-03-04 05:14:46 +0530

    Error Handling

---
+++ Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/exception/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.siva.apps.springboot.helloworld.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private  String message;
+    private  String details;
+}
