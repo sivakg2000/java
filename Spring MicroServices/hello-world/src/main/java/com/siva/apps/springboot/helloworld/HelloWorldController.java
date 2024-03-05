e500b97d493e969c3e887e8a8f2b10ed4c80de40
sivakg2000 <sivakg2000@gmail.com>
2024-03-05 05:30:30 +0530

    swagger,cont-nego,i18n

--- Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/HelloWorldController.java
+++ Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.siva.apps.springboot.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -23,4 +34,14 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World "+name);
     }
+
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldI18n() {
+        Locale locale=LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message", locale);
+        //return "Hello World -i18n";
+    }
+
+
 }
