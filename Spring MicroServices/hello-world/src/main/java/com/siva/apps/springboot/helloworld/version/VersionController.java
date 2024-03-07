45531dde5f56bf5f745df352d3ee8208e2ad3603
sivakg2000 <sivakg2000@gmail.com>
2024-03-07 11:19:54 +0530

    Update VersionController.java

--- Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/version/VersionController.java
+++ Spring MicroServices/hello-world/src/main/java/com/siva/apps/springboot/helloworld/version/VersionController.java
@@ -33,4 +33,34 @@ public class VersionController {
 
 
 
+    @GetMapping(path = "/person/header",headers = "X-API-VERSION=1")
+    public PersonV1 getPersonV1Header(){
+        return new PersonV1("Siva");
+    }
+
+
+
+    @GetMapping(path = "/person/header",headers = "X-API-VERSION=2")
+    public PersonV2 getPersonV2Header(){
+        return new PersonV2(new PersonV2.Name("Siva","Karuppiah"));
+    }
+
+
+
+
+    @GetMapping(path = "/person/accept",produces = "application/test.app-v1+json")
+    public PersonV1 getPersonV1HeaderAccept(){
+        return new PersonV1("Siva");
+    }
+
+
+
+    @GetMapping(path = "/person/accept",produces = "application/test.app-v2+json")
+    public PersonV2 getPersonV2HeaderAccep(){
+        return new PersonV2(new PersonV2.Name("Siva","Karuppiah"));
+    }
+
+
+
+
 }
