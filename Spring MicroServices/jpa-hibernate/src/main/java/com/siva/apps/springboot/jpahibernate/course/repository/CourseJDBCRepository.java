efb45c1e3d4b68b7f785034440daf1b4622354a1
sivakg2000 <sivakg2000@gmail.com>
2024-03-15 06:24:33 +0530

    CourseJDBCRepository update,delete,find by id

--- Spring MicroServices/jpa-hibernate/src/main/java/com/siva/apps/springboot/jpahibernate/course/repository/CourseJDBCRepository.java
+++ Spring MicroServices/jpa-hibernate/src/main/java/com/siva/apps/springboot/jpahibernate/course/repository/CourseJDBCRepository.java
@@ -1,7 +1,9 @@
 package com.siva.apps.springboot.jpahibernate.course.repository;
 
 
+import com.siva.apps.springboot.jpahibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +15,32 @@ public class CourseJDBCRepository {
 
 
     private static String INSERT_Q = """
-                INSERT INTO COURSE(id,name,author) VALUES(1,'AWS Fundamentals','Siva CR');
+                INSERT INTO COURSE(id,name,author) VALUES(?,?,?);
+            """;
+    private static String DELETE_Q = """
+                DELETE FROM COURSE WHERE id=?;
             """;
 
-    public void insert() {
-        jdbcTemplate.update(INSERT_Q);
+    private static String UPDATE_Q = """
+                UPDATE COURSE SET name=?,author=? WHERE id=?;
+            """;
+
+    private static String SELECT_Q = """
+                SELECT * FROM COURSE WHERE id=?;
+            """;
+    public void insert(Course course) {
+        jdbcTemplate.update(INSERT_Q,course.getId(),course.getName(),course.getAuthor());
+    }
+
+    public void deleteById(int id) {
+        jdbcTemplate.update(DELETE_Q,id);
+    }
+
+    public void updateById(Course course) {
+        jdbcTemplate.update(UPDATE_Q,course.getName(),course.getAuthor(),course.getId());
+    }
+
+    public Course findById(int id) {
+        return jdbcTemplate.queryForObject(SELECT_Q,new BeanPropertyRowMapper<>(Course.class),id);
     }
 }
