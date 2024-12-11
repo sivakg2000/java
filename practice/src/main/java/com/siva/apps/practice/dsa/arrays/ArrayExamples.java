c89b07fa63ad8cbef3469ad7ba6995adc1fc90dc
sivakg2000 <sivakg2000@gmail.com>
2024-12-11 17:34:26 +0530

    arrays

---
+++ practice/src/main/java/com/siva/apps/practice/dsa/arrays/ArrayExamples.java
@@ -0,0 +1,13 @@
+package com.siva.apps.practice.dsa.arrays;
+
+public class ArrayExamples {
+    public static void main(String[] args){
+        int[] numbers=new int[10];
+
+        for(int number:numbers){
+            System.out.println(number);
+        }
+
+
+    }
+}
