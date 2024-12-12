6f4f87bef759c9f0d90bec175379c86e99c5c37b
sivakg2000 <sivakg2000@gmail.com>
2024-12-12 06:08:12 +0530

    Update ArrayExamples.java

--- practice/src/main/java/com/siva/apps/practice/dsa/arrays/ArrayExamples.java
+++ practice/src/main/java/com/siva/apps/practice/dsa/arrays/ArrayExamples.java
@@ -10,12 +10,18 @@ public class ArrayExamples {
         System.out.println("Max getMaxConsecutive "+ Arrays.toString(numbers1) +", Result :"+result1);
         int[] numbers2={12,13,345,2,6,7896};
         int result2=arrayExamples.findNumbers(numbers2);
-        System.out.println(" findNumbers "+ Arrays.toString(numbers2) +", Result :"+result2); */
+        System.out.println("findNumbers "+ Arrays.toString(numbers2) +", Result :"+result2);
 
 
         int[] numbers3={-4,-1,0,3,10};
         int[] result3=arrayExamples.sortedSquares(numbers3);
-        System.out.println("sortedSquares "+ Arrays.toString(numbers3) +", Result :"+ Arrays.toString(result3));
+        System.out.println("sortedSquares "+ Arrays.toString(numbers3) +", Result :"+ Arrays.toString(result3));*/
+
+ 
+        arrayExamples.insertElement(-1,1000,new int[10]);
+        arrayExamples.insertElement(0,1000,new int[10]);
+        arrayExamples.insertElement(2,1000,new int[10]);
+        arrayExamples.insertElement(33,1000,new int[10]);
 
     }
 
@@ -62,5 +68,37 @@ public class ArrayExamples {
         }
         Arrays.sort(result);
         return result;
+    }
+
+
+    public void insertElement(int position,int newElement,int[] nums){
+
+
+
+        System.out.println("Before "+ Arrays.toString(nums));
+
+        int cPosition = 0;
+        for (int i = 0; i < 5; i++) {
+            nums[i] = i + 1;
+            cPosition++;
+        }
+
+            if(position == -1)
+            {
+                nums[cPosition] = newElement;
+
+            } else if (position>=0 && position<nums.length){
+                for (int i = 5; i >=position ; i--) {
+                    nums[i+1] = nums[i];
+
+                }
+                nums[position] = newElement;
+
+            }else{
+                System.out.println("Out of bound error");
+            }
+
+
+        System.out.println("Result :"+ Arrays.toString(nums));
     }
 }
